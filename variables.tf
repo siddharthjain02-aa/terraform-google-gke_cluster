3cef9d6d0eca621ce798694f1438938fb6a63ca7
siddharthjain <siddharthjain@airasia.com>
2026-05-11 15:07:49 +0800

    semantic

--- variables.tf
+++ variables.tf
@@ -421,9 +421,7 @@ variable "nginx_controller" {
     replica_count  = optional(number, 2)
     memory_request = optional(string, "256Mi")
     memory_limit   = optional(string, "512Mi")
-    extraArgs = {
-      update-sttus = optional(bool, true)
-    }
+    extraArgs      = optional(map)
   })
   default = {
     enabled = false
